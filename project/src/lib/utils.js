1d15501ca708711592372c081bd763c5e10f0050
Eric Muckley <22535087+ericmuckley@users.noreply.github.com>
2023-12-21 14:40:33 -0800

    linting

--- project/src/lib/utils.js
+++ project/src/lib/utils.js
@@ -1,5 +1,6 @@
-export const NULL_ADDRESS = "0x0000000000000000000000000000000000000000000000000000000000000000";
+export const NULL_ADDRESS =
+  "0x0000000000000000000000000000000000000000000000000000000000000000";
 
 export const shortHash = (s, n = 6) => {
-    return `${s.slice(0, n)}...${s.slice(s.length - n, s.length)}`;
+  return `${s.slice(0, n)}...${s.slice(s.length - n, s.length)}`;
 };
